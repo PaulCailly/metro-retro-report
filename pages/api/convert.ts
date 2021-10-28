db2d8596d365f3671bb05e6f225b6cb45386fe39
Paul Cailly <pcailly@pm.me>
2021-10-28 18:08:17 +0200

    switch to memory storage

--- pages/api/convert.ts
+++ pages/api/convert.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
 import multer from "multer";
-import fs from "fs";
 
 type SuccessfulResponse<T> = { data: T; error?: never; statusCode?: number };
 type UnsuccessfulResponse<E> = { data?: never; error: E; statusCode?: number };
@@ -11,16 +10,13 @@ type ApiResponse<T, E = unknown> =
   | UnsuccessfulResponse<E>;
 
 interface NextConnectApiRequest extends NextApiRequest {
-  files: Express.Multer.File[];
+  file: Express.Multer.File;
 }
 type ResponseData = ApiResponse<string, string>;
 
 const upload = multer({
   limits: { fileSize: 2000000 },
-  storage: multer.diskStorage({
-    destination: "./public",
-    filename: (_req, file, cb) => cb(null, "report.json"),
-  }),
+  storage: multer.memoryStorage(),
   fileFilter: (_req, file, cb) => {
     const acceptFile: boolean = ["application/json"].includes(file.mimetype);
 
@@ -31,7 +27,7 @@ const upload = multer({
 const route = nextConnect({
   onError(
     error,
-    req: NextConnectApiRequest,
+    _req: NextConnectApiRequest,
     res: NextApiResponse<ResponseData>
   ) {
     res.status(501).json({ error: error.message });
@@ -41,24 +37,22 @@ const route = nextConnect({
   },
 });
 
-route.use(upload.array("report"));
+route.use(upload.single("report"));
 
-route.post(
-  (_req: NextConnectApiRequest, res: NextApiResponse<ResponseData>) => {
-    let result = "";
+route.post((req: NextConnectApiRequest, res: NextApiResponse<ResponseData>) => {
+  let result = "";
 
-    const data = fs.readFileSync("public/report.json", "utf8");
-    const json = JSON.parse(data);
-    Object.keys(json).forEach((category: any) => {
-      result = result.concat("\n\n", category);
-      json[category].forEach((item: any) => {
-        result = result.concat("\n", `- ${item.content}`);
-      });
+  const data = req.file.buffer.toString("utf8");
+  const json = JSON.parse(data);
+  Object.keys(json).forEach((category: any) => {
+    result = result.concat("\n\n", category);
+    json[category].forEach((item: any) => {
+      result = result.concat("\n", `- ${item.content}`);
     });
+  });
 
-    res.status(200).json({ data: result });
-  }
-);
+  res.status(200).json({ data: result });
+});
 
 export const config = {
   api: {
