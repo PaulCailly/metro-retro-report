5bdd549f15b1fc08573202304e39eb71c0b8d996
pcailly <pcailly@pm.me>
2021-10-29 01:23:13 +0200

    add emojis

--- pages/api/convert.ts
+++ pages/api/convert.ts
@@ -40,15 +40,96 @@ const route = nextConnect({
 route.use(upload.single("report"));
 
 route.post((req: NextConnectApiRequest, res: NextApiResponse<ResponseData>) => {
-  let result = "";
-
   const data = req.file.buffer.toString("utf8");
   const json = JSON.parse(data);
-  Object.keys(json).forEach((category: any) => {
-    result = result.concat("\n\n", category);
+
+  const categories: any = {
+    Actions: "🎬",
+    Good: "☀️",
+    Bad: "🌧️",
+    Start: "🚀",
+    Stop: "🛑",
+    Liked: "😀",
+    Learned: "🧐",
+    Lacked: "😞",
+    "Longed For": "🤔",
+    "Sunny Skies": "☀️",
+    "Storm Clouds": "⛈️",
+    "Hot Air": "♨️",
+    Sandbags: "🗿",
+    Goal: "🎯",
+    Wind: "💨",
+    Rocks: "🗿",
+    Anchor: "⚓",
+    Engine: "⚙️",
+    Parachute: "🪂",
+    "More Of": "☑️",
+    "Less Of": "❌",
+    "Keep Doing": "♻️",
+    "Start Doing": "🚀",
+    "Stop Doing": "🛑",
+    Sidekick: "🙋",
+    Weakness: "😓",
+    "Super Power": "💪",
+    Added: "☑️",
+    Removed: "❌",
+    Recycle: "♻️",
+    "Didn't Work": "📉",
+    "Kinda Worked": "📊",
+    "Worked Well": "📈",
+    Mad: "😡",
+    Glad: "😀",
+    Sad: "😭",
+    Drop: "🎤",
+    Improve: "🏗️",
+    Keep: "♻️",
+    Add: "☑️",
+    Continue: "♻️",
+    Plus: "☑️",
+    Interesting: "🤔",
+    Minus: "❌",
+    Recognition: "🏆",
+    "Thumbs Down": "👎",
+    "Thumbs Up": "👍",
+    "New Ideas": "💡",
+    Bridge: "🌉",
+    Abyss: "⚠️",
+    Hopes: "🙏",
+    Concerns: "😟",
+    Risks: "⚠️",
+    Issues: "🔥",
+    Assumptions: "🙈",
+    Dependencies: "🔗",
+    Strengths: "💪",
+    Weaknesses: "😓",
+    Opportunities: "💡",
+    Threats: "⚠️",
+  };
+
+  let result = "";
+
+  const appendCategory = (category: string) => {
+    result = result.concat("\n\n", `${categories[category]} ${category}`);
+  };
+
+  const appendItems = (category: string) => {
     json[category].forEach((item: any) => {
       result = result.concat("\n", `- ${item.content}`);
     });
+  };
+
+  Object.keys(json).forEach((category: string) => {
+    if (category !== "Actions") {
+      appendCategory(category);
+      appendItems(category);
+    }
+  });
+
+  Object.keys(json).forEach((category: string) => {
+    if (category === "Actions") {
+      appendCategory(category);
+      appendItems(category);
+    }
   });
 
   res.status(200).json({ data: result });
